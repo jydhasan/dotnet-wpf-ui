4a1f549ce0eb4c8f5ce66cf53a797e3ee49adfe6
jydhasan <s1910933181@ru.ac.bd>
2026-09-11 11:15:13 +0600

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Wpf.Ui.Appearance;
+
+namespace ModernUI
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApplicationThemeManager.Apply(this);
+        }
+    }
+}
